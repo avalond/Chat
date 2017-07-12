7f5a313fb45ff47392f493e88bd21268b99980a9
kevin <agonyice0115@gmail.com>
2017-07-12 15:24:29 +0800

    add server

---
+++ server/data/mocks.js
@@ -0,0 +1,8 @@
+/**
+ * Created by kevin on 7/12/17.
+ */
+
+export const Mocks = {
+  String: () => 'It works'
+};
+export default Mocks;
--- server/index.js
+++ server/index.js
@@ -2,8 +2,34 @@
  * Created by kevin on 7/12/17.
  */
 import express from 'express'
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import {makeExecutableSchema, addMockFunctionsToSchema} from 'graphql-tools';
+import bodayParser from 'body-parser';
+import {createServer} from 'http';
+import {Schema} from './data/schema';
+import {Mocks} from './data/mocks';
 
 const PORT = 8080;
 const app = express();
 
-app.listen(PORT, console.log(`server is running on http://localhost:${PORT}`));
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema
+});
+
+addMockFunctionsToSchema({
+  schema: executableSchema,
+  mocks: Mocks,
+  preserveResolvers: true
+});
+
+app.use('/graphql', bodayParser.json(), graphqlExpress({
+  schema: executableSchema,
+  context: {}, // at least(!) an empty object
+}));
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
+
+const graphQLServer = createServer(app);
+
+graphQLServer.listen(PORT, console.log(`server is running on http://localhost:${PORT}/graphiql`));
